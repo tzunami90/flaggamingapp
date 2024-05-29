26de6a5cf9dbe6eaebec21969b7df3f4dbbd8117
Alejandro SOlodujin <solodujina.cl@gmail.com>
2024-05-29 18:41:05 -0300

    - Se agrego spinner de categorias para filtrar por categoria en lista de hard - Se modifico para que primero cargue cateogira y luego productos por el spinner - Se modifico el progress bar y tiene metodos dependiendo se ya cargo el spinner y el producto.

--- app/src/main/java/com/beone/flagggaming/producto/ListaHardware.java
+++ app/src/main/java/com/beone/flagggaming/producto/ListaHardware.java
@@ -4,7 +4,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,6 +34,9 @@ public class ListaHardware extends AppCompatActivity {
     private ProductoClienteAdapter productoClienteAdapter;
     private List<Producto> productoList;
     private List<Producto> filteredList;
+    private ProgressBar progressBar;
+    private boolean categoriesLoaded = false;
+    private boolean productsLoaded = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,11 +45,17 @@ public class ListaHardware extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerViewProductos);
         SearchView searchView = findViewById(R.id.searchView);
+        Spinner spinnerCategorias = findViewById(R.id.spinnerCategorias);
+        progressBar = findViewById(R.id.progressBar);
 
         productoList = new ArrayList<>();
         filteredList = new ArrayList<>();
 
-        new LoadProductsTask().execute();
+        // Mostrar el ProgressBar al inicio
+        showProgressBar();
+
+        // Cargar las categorías y productos
+        new LoadCategoriesTask().execute();
 
         filteredList.addAll(productoList);
         productoClienteAdapter = new ProductoClienteAdapter(filteredList, this);
@@ -58,8 +70,19 @@ public class ListaHardware extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                filtrarProductos(newText);
+                filtrarProductos(newText,  (Categoria) spinnerCategorias.getSelectedItem());
                 return true;
+            }
+        });
+
+        spinnerCategorias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                filtrarProductos(searchView.getQuery().toString(), (Categoria) parent.getSelectedItem());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
             }
         });
 
@@ -71,14 +94,17 @@ public class ListaHardware extends AppCompatActivity {
         });
     }
 
-    private void filtrarProductos(String texto) {
+    private void filtrarProductos(String texto, Categoria categoria) {
         filteredList.clear();
-        if (TextUtils.isEmpty(texto)) {
+        if (TextUtils.isEmpty(texto) && (categoria == null || categoria.getId_categoria() == 0)) {
             filteredList.addAll(productoList);
         } else {
             for (Producto producto : productoList) {
-                if (producto.getDescTienda().toLowerCase().contains(texto.toLowerCase()) ||
-                        producto.getMarca().toLowerCase().contains(texto.toLowerCase())) {
+                boolean matchesText = producto.getDescTienda().toLowerCase().contains(texto.toLowerCase()) ||
+                        producto.getMarca().toLowerCase().contains(texto.toLowerCase());
+                boolean matchesCategory = categoria == null || producto.getIdCategoria() == categoria.getId_categoria();
+
+                if (matchesText && matchesCategory) {
                     filteredList.add(producto);
                 }
             }
@@ -86,13 +112,61 @@ public class ListaHardware extends AppCompatActivity {
         productoClienteAdapter.notifyDataSetChanged();
     }
 
-    private class LoadProductsTask extends AsyncTask<Void, Void, List<Producto>> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            ProgressBar progressBar = findViewById(R.id.progressBar);
-            progressBar.setVisibility(View.VISIBLE);
+    private void showProgressBar() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        if (categoriesLoaded && productsLoaded) {
+            progressBar.setVisibility(View.GONE);
         }
+    }
+    private class LoadCategoriesTask extends AsyncTask<Void, Void, List<Categoria>> {
+        @Override
+        protected List<Categoria> doInBackground(Void... voids) {
+            List<Categoria> categorias = new ArrayList<>();
+            try {
+                Connection connection = DBHelper.conDB(ListaHardware.this);
+                if (connection != null) {
+                    String query = "SELECT id_categoria, desc_categoria FROM categorias";
+                    PreparedStatement statement = connection.prepareStatement(query);
+                    ResultSet resultSet = statement.executeQuery();
+
+                    categorias.add(new Categoria(0, "Todas las categorías")); // Añadir opción por defecto
+
+                    while (resultSet.next()) {
+                        int idCategoria = resultSet.getInt("id_categoria");
+                        String descCategoria = resultSet.getString("desc_categoria");
+                        Categoria categoria = new Categoria(idCategoria, descCategoria);
+                        categorias.add(categoria);
+                    }
+
+                    resultSet.close();
+                    statement.close();
+                    connection.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return categorias;
+        }
+
+        @Override
+        protected void onPostExecute(List<Categoria> categorias) {
+            Spinner spinnerCategorias = findViewById(R.id.spinnerCategorias);
+            ArrayAdapter<Categoria> adapter = new ArrayAdapter<>(ListaHardware.this, android.R.layout.simple_spinner_item, categorias);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinnerCategorias.setAdapter(adapter);
+
+            categoriesLoaded = true;
+            hideProgressBar();
+
+            // Cargar los productos después de configurar el Spinner
+            new LoadProductsTask().execute();
+        }
+    }
+
+    private class LoadProductsTask extends AsyncTask<Void, Void, List<Producto>> {
         @Override
         protected List<Producto> doInBackground(Void... voids) {
             List<Producto> productos = new ArrayList<>();
@@ -136,8 +210,9 @@ public class ListaHardware extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(List<Producto> productos) {
-            ProgressBar progressBar = findViewById(R.id.progressBar);
-            progressBar.setVisibility(View.GONE);
+            productsLoaded = true;
+            hideProgressBar();
+
             productoList.clear();
             productoList.addAll(productos);
             filteredList.clear();
